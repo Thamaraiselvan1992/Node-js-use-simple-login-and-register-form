7ae6fb4d9b485872de3ecd77f35c1046e505da57
Thamaraiselvan1992 <45159211+Thamaraiselvan1992@users.noreply.github.com>
2019-05-07 12:22:52 +0530

    Update db.js

--- model/db.js
+++ model/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://Thamaraiselvan:Thamarai@123@cluster0-66r7n.mongodb.net/dashboard", { useNewUrlParser: true }, (err) => {
+mongoose.connect("//mongodb url", { useNewUrlParser: true }, (err) => {
     if (!err) {
         console.log('Sucessfully connected')
     } else {
