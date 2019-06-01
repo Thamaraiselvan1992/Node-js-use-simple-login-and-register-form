6ed75b7b058ec9c6a821a948da5a5bc047dea410
Thamaraiselvan1992 <tsthamarai4@gmail.com>
2019-06-01 10:47:13 +0530

    second commit

--- model/db.js
+++ model/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("//mongodb url", { useNewUrlParser: true }, (err) => {
+mongoose.connect("mongodb+srv://Thamaraiselvan:"+process.env.MongoDB_PASS+"@cluster0-66r7n.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true }, (err) => {
     if (!err) {
         console.log('Sucessfully connected')
     } else {
